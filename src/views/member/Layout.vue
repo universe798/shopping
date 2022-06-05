72166c49bed7738583da0af93f7695cd77343b04
ccc <cph@qq.com>
2022-06-05 11:58:24 +0800

    电商初始化

---
+++ src/views/member/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>顶</nav>
+     
+    <header>头</header>
+
+    <div class="main">
+        <RouterView />
+    </div>
+
+    <footer>脚</footer>
+</template>
+
+<script>
+    export default {
+        name: 'Layout'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
